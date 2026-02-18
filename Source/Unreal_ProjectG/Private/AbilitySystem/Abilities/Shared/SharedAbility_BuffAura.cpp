636a321d8f22ff95a308f34a2ebd568bdf99acf9
LDY <ldsldy@naver.com>
2026-02-18 22:12:07 +0900

    주변에 버프를 주는 오라 어빌리티 구현 중

---
+++ Source/Unreal_ProjectG/Private/AbilitySystem/Abilities/Shared/SharedAbility_BuffAura.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/Shared/SharedAbility_BuffAura.h"
+
---
+++ Source/Unreal_ProjectG/Public/AbilitySystem/Abilities/Shared/SharedAbility_BuffAura.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/PGGameplayAbility.h"
+#include "SharedAbility_BuffAura.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_PROJECTG_API USharedAbility_BuffAura : public UPGGameplayAbility
+{
+	GENERATED_BODY()
+	
+};

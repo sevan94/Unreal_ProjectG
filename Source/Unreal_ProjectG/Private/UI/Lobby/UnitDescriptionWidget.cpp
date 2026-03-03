abee99992a56851e10ba87b9b3b7b3529f0fa1e7
eyeofsol <sjh991101@gmail.com>
2026-03-03 13:33:52 +0900

    유닛 설명 바인딩

--- Source/Unreal_ProjectG/Private/UI/Lobby/UnitDescriptionWidget.cpp
+++ Source/Unreal_ProjectG/Private/UI/Lobby/UnitDescriptionWidget.cpp
@@ -13,26 +13,43 @@ void UUnitDescriptionWidget::UpdateDescription(UUnitUIDataAsset* InData)
     // 유닛 기본 정보 설정
     UnitName->SetText(FText::FromName(InData->UnitName));
     UnitImage->SetBrushFromTexture(InData->UnitImage);
+    UnitCost->SetText(FText::AsNumber(InData->UnitCost));
 
     static const FString ContextString(TEXT("Unit Stat Context"));
 
     FName HealthRowName = *FString::Printf(TEXT("%sMaxHealth"), *InData->UnitStatus);
-    FName AttackRowName = *FString::Printf(TEXT("%sAttackPower"), *InData->UnitStatus);
+    FName AttackDamageRowName = *FString::Printf(TEXT("%sAttackDamage"), *InData->UnitStatus);
+    FName AttackSpeedRowName = *FString::Printf(TEXT("%sAttackSpeed"), *InData->UnitStatus);
+    FName MoveSpeedRowName = *FString::Printf(TEXT("%sMoveSpeed"), *InData->UnitStatus);
 
-    // 2. 커브 찾기
+    // 커브 찾기
     FRealCurve* HealthCurve = StatusCurveTable->FindCurve(HealthRowName, ContextString);
-    FRealCurve* AttackPowerCurve = StatusCurveTable->FindCurve(AttackRowName, ContextString);
+    FRealCurve* AttackDamageCurve = StatusCurveTable->FindCurve(AttackDamageRowName, ContextString);
+    FRealCurve* AttackSpeedCurve = StatusCurveTable->FindCurve(AttackSpeedRowName, ContextString);
+    FRealCurve* MoveSpeedCurve = StatusCurveTable->FindCurve(MoveSpeedRowName, ContextString);
 
-    // 3. 데이터 적용
+    // 데이터 적용
     if (HealthCurve)
     {
         float HealthValue = HealthCurve->Eval(1.0f);
         UnitHealth->SetText(FText::AsNumber(HealthValue));
     }
 
-    if (AttackPowerCurve)
+    if (AttackDamageCurve)
     {
-        float AttackValue = AttackPowerCurve->Eval(1.0f);
-        // UnitAttack->SetText(FText::AsNumber(AttackValue)); // UI에 맞게 추가
+        float AttackDamageValue = AttackDamageCurve->Eval(1.0f);
+        UnitAttackDamage->SetText(FText::AsNumber(AttackDamageValue));
+    }
+
+    if (AttackSpeedCurve)
+    {
+        float AttackSpeedValue = AttackSpeedCurve->Eval(1.0f);
+        UnitAttackSpeed->SetText(FText::AsNumber(AttackSpeedValue));
+    }
+
+    if (MoveSpeedCurve)
+    {
+        float MoveSpeedValue = MoveSpeedCurve->Eval(1.0f);
+        UnitMoveSpeed->SetText(FText::AsNumber(MoveSpeedValue));
     }
 }

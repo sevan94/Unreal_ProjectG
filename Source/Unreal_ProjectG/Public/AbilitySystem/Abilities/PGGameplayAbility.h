17f514e66594ac41544c75437cd1caeac9b0d6a7
LDY <137361731+ldsldy@users.noreply.github.com>
2026-03-18 18:08:17 +0900

    uI에 사용할 발동 정책  위치 변경

--- Source/Unreal_ProjectG/Public/AbilitySystem/Abilities/PGGameplayAbility.h
+++ Source/Unreal_ProjectG/Public/AbilitySystem/Abilities/PGGameplayAbility.h
@@ -58,6 +58,12 @@ protected:
     UFUNCTION(BlueprintPure)
     FGameplayEffectSpecHandle MakeDurationStatusEffectSpecHandle(TSubclassOf<UGameplayEffect> EffectClass, float Duration);
 
+public:
+    // 일단은 UI 사용 용도로 Public하게 두었음, 추후 로직에 들어가면 위치 변경 가능
+    // FAbilityEntry 안에 둔것이 아니기 때문에, 스킬 발동 방식은 Upgrade와 기본 스킬 모두 공통되어야 함. 추후, 생각 바뀌면 이동 가능
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (DisplayName = "스킬 발동 방식"))
+    ESkillActivationType ActivationType = ESkillActivationType::Instant;
+
 protected:
     UPROPERTY(EditDefaultsOnly, Category = "PGGameplayAbility")
     EPGAbilityActivationPolicy AbilityActivationPolicy = EPGAbilityActivationPolicy::OnTriggered;

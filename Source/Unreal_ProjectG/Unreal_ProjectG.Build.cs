9ce2ff271940381879323fd6fd07be055ff2fff9
LDY <137361731+ldsldy@users.noreply.github.com>
2026-03-23 09:47:33 +0900

    Revert "Merge branch 'main' into LDY_HeroSkillData"
    This reverts commit a5e1c90add524cbf81048aec8d56341469e2a24a, reversing
    changes made to c9bce6773f78885c6d6d417efabdcd1d57f39a03.

--- Source/Unreal_ProjectG/Unreal_ProjectG.Build.cs
+++ Source/Unreal_ProjectG/Unreal_ProjectG.Build.cs
@@ -8,7 +8,7 @@ public class Unreal_ProjectG : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "NavigationSystem", "AIModule",
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "NavigationSystem",
         "UMG", "GameplayTags", "GameplayAbilities", "GameplayTasks", "Niagara", "Slate", "SlateCore"});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });

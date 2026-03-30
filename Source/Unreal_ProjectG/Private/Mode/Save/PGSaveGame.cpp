c2d6f40c949beaf3898c4f4c0eb7c6ecba3b2aee
eyeofsol <sjh991101@gmail.com>
2026-03-30 10:19:52 +0900

    테스트용 초기 재화 설정

--- Source/Unreal_ProjectG/Private/Mode/Save/PGSaveGame.cpp
+++ Source/Unreal_ProjectG/Private/Mode/Save/PGSaveGame.cpp
@@ -8,9 +8,9 @@ UPGSaveGame::UPGSaveGame()
     // 여기서 기본값을 초기화
     PlayerName = TEXT("Player");
     PlayerLevel = 1;
-    PlayerGold = 0;
-    PlayerGem = 0;
-    PlayerUnlock = 0;
+    PlayerGold = 1000;
+    PlayerGem = 500;
+    PlayerUnlock = 500;
 
     //사운드
     MasterVolume = 1.0f;
